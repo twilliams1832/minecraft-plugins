a29efa6343e67c25d7c38b7c4ce9b91999efeae7
twilliams1832 <twilliams1832@gmail.com>
2025-10-05 14:38:18 -0500

    Updated mount script

--- sirenhead/scripts/mount_share.sh
+++ sirenhead/scripts/mount_share.sh
@@ -8,8 +8,8 @@ USERNAME="$4"
 PASSWORD="$5"
 
 echo "🔧 Mounting //${SERVER}/${SHARE} to ${MOUNT_PATH}"
-mkdir -p "$MOUNT_PATH"
-mount -t cifs "//${SERVER}/${SHARE}" "$MOUNT_PATH" \
+sudo mkdir -p "$MOUNT_PATH"
+sudo mount -t cifs "//${SERVER}/${SHARE}" "$MOUNT_PATH" \
     -o username="$USERNAME",password="$PASSWORD",rw,uid=$(id -u),gid=$(id -g)
 
 mountpoint -q "$MOUNT_PATH" || {
