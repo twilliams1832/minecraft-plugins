f0e2928dd9a66ef81b62438678b5f1f1ef448b08
twilliams1832 <twilliams1832@gmail.com>
2025-10-05 02:00:58 -0500

    Updated jenkinsfile. Added helper scripts

---
+++ sirenhead/scripts/mount_share.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+SERVER="$1"
+SHARE="$2"
+MOUNT_PATH="$3"
+USERNAME="$4"
+PASSWORD="$5"
+
+echo "🔧 Mounting //${SERVER}/${SHARE} to ${MOUNT_PATH}"
+mkdir -p "$MOUNT_PATH"
+mount -t cifs "//${SERVER}/${SHARE}" "$MOUNT_PATH" \
+    -o username="$USERNAME",password="$PASSWORD",rw,uid=$(id -u),gid=$(id -g)
+
+mountpoint -q "$MOUNT_PATH" || {
+    echo "❌ Mount failed"
+    exit 1
+}
